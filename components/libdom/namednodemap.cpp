cc994326112429f00a42bd918eed6500cd77da5d
Victor B. <contact@itsvic.dev>
2023-12-20 17:03:09 +0100

    [libdom] better setNamedItem impl

--- components/libdom/namednodemap.cpp
+++ components/libdom/namednodemap.cpp
@@ -1,4 +1,6 @@
 #include "libdom/namednodemap.h"
+#include <algorithm>
+#include <iterator>
 namespace LibDOM {
 
 unsigned long NamedNodeMap::length() { return m_attrs.size(); }
@@ -7,8 +9,18 @@ std::shared_ptr<Attr> NamedNodeMap::item(unsigned long index) {
 }
 
 std::shared_ptr<Attr> NamedNodeMap::setNamedItem(std::shared_ptr<Attr> attr) {
-  m_attrs.push_back(attr);
-  return attr;
+  std::shared_ptr<Attr> ret;
+  auto it = std::find_if(
+      m_attrs.begin(), m_attrs.end(),
+      [attr](std::shared_ptr<Attr> a1) { return a1->name == attr->name; });
+  if (it == m_attrs.end()) {
+    ret = attr;
+    m_attrs.push_back(attr);
+  } else {
+    ret = *it;
+    ret->value = attr->value;
+  }
+  return ret;
 }
 
 } // namespace LibDOM
